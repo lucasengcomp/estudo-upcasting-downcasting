786c73d68005ba6307a6d7357a3db30f3dc5e247
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2021-09-26 19:48:28 -0300

    Upcasting

---
+++ README.md
@@ -0,0 +1,10 @@
+Projeto criado apenas para estudos sobre Upcasting e Downcasting de Classes.
+Relembrando conceitos:
+Upcasting: faz o casting da subclasse para a superclasse.
+- polimorfismo usa.
+Downcasting: faz o casting da superclasse para a subclasse.
+- uso de instanceof.
+- comumente usado nos métodos que recebem parâmetro genéricos por exemplo o método equals.
+
+
+ 
--- src/com/br/Main.java
+++ src/com/br/Main.java
@@ -1,9 +1,22 @@
 package com.br;
 
+import com.br.entities.Account;
+import com.br.entities.BusinessAccount;
+import com.br.entities.SavingsAccount;
+
 public class Main {
 
     public static void main(String[] args) {
-	
 
+        Account acc = new Account(1001, "Alex", 0.0);
+        BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+        //UPCASTING
+        Account acc1 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+        Account acc2 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+
+        System.out.println(acc);
+        System.out.println(acc1);
+        System.out.println(acc2);
     }
 }
